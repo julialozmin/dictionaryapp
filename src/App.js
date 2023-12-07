e15ee9a707fb2fb7044a163d4b039f9117c418bd
julialozmin <julialozmin@gmail.com>
2023-12-07 18:52:26 +0100

    Added some structure to the page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>dictionary</h1>
+        <h5>
+          A picture is worth a thousand words, but you cannot explain pictures
+          without words
+        </h5>
+      </header>
+      <form>
+        <input type="search" placeholder="..."></input>
+        <input type="submit">Search Word</input>
+      </form>
     </div>
   );
 }
