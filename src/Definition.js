eca3d68819f444f59453519c0a7e3380725586d4
julialozmin <julialozmin@gmail.com>
2023-12-12 11:14:45 +0100

    Added the Synonyms component

--- src/Definition.js
+++ src/Definition.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 import "./Definition.css";
 
 export default function Definition(props) {
   console.log(props.meanings);
   console.log(props.meanings.partOfSpeech);
+  console.log(props.meanings.synonyms);
 
   return (
     <div className="Definition">
@@ -14,6 +16,8 @@ export default function Definition(props) {
           <div key={index}>
             <p className="singleDefinition">{definition.definition}</p>
             <p className="example">{definition.example}</p>
+            <Synonyms synonyms={definition.synonyms} />
+            {/* <p className="synonyms">{definition.synonyms}</p> */}
           </div>
         );
       })}
