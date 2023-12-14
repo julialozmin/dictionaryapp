28c5a550096b7e46002bea858b5c8f482f6f0db5
julialozmin <julialozmin@gmail.com>
2023-12-14 12:51:05 +0100

    function updates for deploy

--- src/ImagesDisplay.js
+++ src/ImagesDisplay.js
@@ -7,19 +7,17 @@ export default function ImagesDisplay(props) {
     return (
       <section className="ImagesDisplay">
         {props.data.map(function (image, index) {
-          if (index < 5) {
-            return (
-              <div className="image" key={index}>
-                <a href={image.src.original} target="_blank" rel="noreferrer">
-                  <img
-                    src={image.src.landscape}
-                    alt={image.alt}
-                    className="img-fluid"
-                  />
-                </a>
-              </div>
-            );
-          }
+          return (
+            <div className="image" key={index}>
+              <a href={image.src.original} target="_blank" rel="noreferrer">
+                <img
+                  src={image.src.landscape}
+                  alt={image.alt}
+                  className="img-fluid"
+                />
+              </a>
+            </div>
+          );
         })}
       </section>
     );
